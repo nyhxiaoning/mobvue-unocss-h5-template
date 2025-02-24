876b0f72857232a9a8356a8065f627845f2d43f4
ningyongheng <ningyongheng@jeejio.com>
2025-02-24 10:19:16 +0800

    feat: 翻译当前的优化说明：修改5处内容

--- src/pages/SwitchPage.vue
+++ src/pages/SwitchPage.vue
@@ -68,15 +68,15 @@ export default {
             },
             language: {
                 enableSwitch: JeeWeb.Language === 'zh-CN' ? '开启切换应用' : 'Enable App Switching',
-                displayApps: JeeWeb.Language === 'zh-CN' ? '水杯显示应用' : 'Cup Display Apps',
+                displayApps: JeeWeb.Language === 'zh-CN' ? '水杯显示应用' : 'App Display',
                 clock: JeeWeb.Language === 'zh-CN' ? '时钟' : 'Clock',
                 deviceInfo: JeeWeb.Language === 'zh-CN' ? '设备信息' : 'Device Info',
                 screensaver: JeeWeb.Language === 'zh-CN' ? '屏保' : 'Screensaver',
                 weather: JeeWeb.Language === 'zh-CN' ? '天气' : 'Weather',
                 ballGame: JeeWeb.Language === 'zh-CN' ? '落球游戏' : 'Gravity',
                 slotGame: JeeWeb.Language === 'zh-CN' ? '老虎机游戏' : 'Slot Machine',
-                shakeEffect: JeeWeb.Language === 'zh-CN' ? '摇晃水杯动效' : 'Cup Shake Effect',
-                darkEmpire: JeeWeb.Language === 'zh-CN' ? '黑幕帝国动效' : 'Dark Empire Effect'
+                shakeEffect: JeeWeb.Language === 'zh-CN' ? '摇晃水杯动效' : 'Shake the cup',
+                darkEmpire: JeeWeb.Language === 'zh-CN' ? '黑幕帝国动效' : 'Matrix'
             }
         };
     },
